eb6b016e69463e57de9f18c84b6fc388753b0a69
Alex <30310397+sergdalm@users.noreply.github.com>
2022-04-14 19:35:19 +0300

    Overrided method PUT in a servlet

--- src/com/sergdalm/http/servlet/FirstServlet.java
+++ src/com/sergdalm/http/servlet/FirstServlet.java
@@ -7,7 +7,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.stream.Stream;
 
 @WebServlet("/first")
 public class FirstServlet extends HttpServlet {
@@ -18,15 +23,38 @@ public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setContentType("text/html");
+        String paramValue = req.getParameter("param");
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        System.out.println();
+
+
+        Enumeration<String> headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String header = headerNames.nextElement();
+            System.out.println(req.getHeader(header));
+        }
+        resp.setContentType("text/html; charset=UTF-8");
+        resp.setHeader("token", "12345");
+//        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
         try (var writer = resp.getWriter()) {
-            writer.write("<h1>Hello from first servlet</h1>");
+            writer.write("<h1>Привет с сервлета! 안녕하세요!</h1>");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        System.out.println(parameterMap);
+
+        try (BufferedReader reader = req.getReader();
+             Stream<String> lines = reader.lines()) {
+            lines.forEach(System.out::println);
         }
     }
 
     @Override
     public void destroy() {
-        super.destroy();
+         super.destroy();
     }
 
 }
