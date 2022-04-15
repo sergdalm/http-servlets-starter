a7bd8f19901fb51e108da0818e046880795d1717
Alex <30310397+sergdalm@users.noreply.github.com>
2022-04-15 21:53:30 +0300

    Added example of using cookies

--- src/com/sergdalm/http/entity/Flight.java
+++ src/com/sergdalm/http/entity/Flight.java
@@ -1,12 +1,10 @@
 package com.sergdalm.http.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
-@AllArgsConstructor
 public class Flight {
     private Long id;
     private String flight_no;
@@ -17,4 +15,14 @@ public class Flight {
     private Integer aircraft_id;
     private FlightStatus status;
 
+    public Flight(Long id, String flight_no, LocalDateTime departure_date, String departure_airport_code, LocalDateTime arrival_data, String arrival_airport_code, Integer aircraft_id, FlightStatus status) {
+        this.id = id;
+        this.flight_no = flight_no;
+        this.departure_date = departure_date;
+        this.departure_airport_code = departure_airport_code;
+        this.arrival_data = arrival_data;
+        this.arrival_airport_code = arrival_airport_code;
+        this.aircraft_id = aircraft_id;
+        this.status = status;
+    }
 }
---
+++ src/com/sergdalm/http/servlet/SessionServlet.java
@@ -0,0 +1,32 @@
+package com.sergdalm.http.servlet;
+
+import com.sergdalm.http.dto.UserDto;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/sessions")
+public class SessionServlet extends HttpServlet {
+    private static final String USER = "user";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        HttpSession session = req.getSession();
+        var user = (UserDto) session.getAttribute(USER);
+
+        // Обычно пользователь устанавливается один раз на странице login
+        // по username и password
+        if(user == null) {
+            user =  UserDto.builder()
+                    .id(25L)
+                    .mail("test@gmail.com")
+                    .build();
+            session.setAttribute(USER, user);
+        }
+    }
+}
