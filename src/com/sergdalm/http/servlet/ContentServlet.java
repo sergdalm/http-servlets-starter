145eacd734cda095f35a6fa653124e92f153a1d0
Alex <30310397+sergdalm@users.noreply.github.com>
2022-04-20 14:21:02 +0300

    Added example of using EL

--- src/com/sergdalm/http/servlet/ContentServlet.java
+++ src/com/sergdalm/http/servlet/ContentServlet.java
@@ -1,5 +1,7 @@
 package com.sergdalm.http.servlet;
 
+import com.sergdalm.http.dto.FlightDto;
+import com.sergdalm.http.service.FlightService;
 import com.sergdalm.http.util.JspHelper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,11 +10,21 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 @WebServlet("/content")
 public class ContentServlet extends HttpServlet {
+    private final FlightService flightService = FlightService.getInstance();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<FlightDto> flightDtos = this.flightService.findAll();
+        req.setAttribute("flights", flightDtos);
+        req.getSession().setAttribute("flightsMap", flightDtos.stream()
+                .collect(toMap(FlightDto::getId, FlightDto::getDescription)));
+
         // В методе getRequestDispatcher() надо указать полный путь к файлу JSP начиная от директории web.
         // Лучше вынести полный путь в утилитный класс (JspHelper)
         req.getRequestDispatcher(JspHelper.getPath("content"))
--- web/WEB-INF/jsp/content.jsp
+++ web/WEB-INF/jsp/content.jsp
@@ -13,7 +13,16 @@
 <body>
     <%@include file="header.jsp"%>
     <div>
-        Content. Русский 한국어
+        <span>Content. Русский 한국어</span>
+        <p>Size: ${requestScope.flights.size()}</p>
+        <p>Id: ${requestScope.flights.get(0).id}</p>
+        <p>Id 2: ${requestScope.flights[1].id}</p>
+        <p>Map: ${sessionScope.flightsMap[1]}</p>
+        <p>JSESSION id: ${cookie["JSESSIONID"].value}, unique identifier</p>
+        <p>Header: ${header["Cookie"]}</p>
+        <p>Param id: ${param.id}</p>
+        <p>Param test: ${param.test}</p>
+        <p>Empty list: ${not empty flights}</p>
     </div>
     <%@include file="footer.jsp"%>
 </body>
